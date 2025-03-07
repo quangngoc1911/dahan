e9d9c7dddb470f43bd676dfb6fd252a4fe2f10d3
Le Quang Ngoc <nhuthung102030@gmail.com>
2025-03-07 16:36:54 +0700

    update UI

--- public/script/index.js
+++ public/script/index.js
@@ -11,28 +11,30 @@ window.onload = function () {
 };
 
 function init() {
-    document.getElementById("clickMe").onclick = function (e) {
-        this.classList.add("closedis");
-        document.getElementById("universe").classList.add("closedis");
-        document.getElementById("firework").classList.add("opendis");
-        document.getElementById("firework").classList.add("backgroundFireword")
-        setTimeout(function () {
-            RunFireWork();
-        },450)
-    }
-    //endmodal
-    document.querySelector("#endmodal").onclick = function () {
-      document.getElementById("framemodal").classList.add("animationclose");
-      setTimeout(function (){
-        document.getElementById("modal").classList.add("closedis"); 
-        setTimeout(function () {
-          document.querySelector("#clickMe").classList.add("animationopen");
-          document.querySelector("#clickMe").classList.remove("closedis");
-        },250)
-      },600)
-    }
+  document.getElementById("clickMe").onclick = function (e) {
+    this.classList.add("closedis");
+    document.getElementById("universe").classList.add("closedis");
+    document.getElementById("firework").classList.add("opendis");
+    document.getElementById("firework").classList.add("backgroundFireword");
+    setTimeout(function () {
+      RunFireWork();
+    }, 450);
+  };
+  //endmodal
+  document.querySelector("#endmodal").onclick = function () {
+    document.getElementById("framemodal").classList.add("animationclose");
+    setTimeout(function () {
+      document.getElementById("modal").classList.add("closedis");
+      setTimeout(function () {
+        document.querySelector("#clickMe").classList.add("animationopen");
+        document.querySelector("#clickMe").classList.remove("closedis");
+      }, 250);
+    }, 600);
+  };
 }
 let name = localStorage.getItem("name");
-document.querySelector("#firework .intro").innerHTML = ` Chúc mừng ngày 8/3 mãi xinh đẹp như bông hồng nhá.
-<br> <p style="font-size:55%;">Riêng ${name} thì chúc những lời chúc tốt đẹp nhất, luôn xinh đẹp, hạnh phúc và vui vẻ không chỉ ngày 8/3 mà cả những ngày khác nữa. </p>  
-`
+document.querySelector(
+  "#firework .intro"
+).innerHTML = ` <div class="text"><img src="../public/img/roseLeft.png" alt="" class="rose" /> <div>Chúc mừng ngày 8/3 mãi xinh đẹp như bông hồng nhá.<br> <p style="font-size:55%;">Chúc ${name} những lời chúc tốt đẹp nhất, luôn xinh đẹp, hạnh phúc và vui vẻ không chỉ ngày 8/3 mà cả những ngày khác nữa. </p> </div> <img src="../public/img/rose.png" alt="" class="rose"/><div>
+ 
+`;
